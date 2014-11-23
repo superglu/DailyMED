190f8cf6a7c3afaf6b61b84f795679a47897bd2e
Zouhair Mahieddine <zouhair.mahieddine@gmail.com>
2014-11-23 13:49:34 -0800

    watch - WatchKit App + Extension + Schemes added

---
+++ rxtime WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  rxtime WatchKit Extension
+//
+//  Created by Zouhair Mahieddine on 11/23/14.
+//  Copyright (c) 2014 Zedenem. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ rxtime WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  rxtime WatchKit Extension
+//
+//  Created by Zouhair Mahieddine on 11/23/14.
+//  Copyright (c) 2014 Zedenem. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
